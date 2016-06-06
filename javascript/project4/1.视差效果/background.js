b32620054cd9159d7a44dcb76d65510b7b7d50bc
soricc <soloxl1991@gmail.com>
2016-06-06 22:27:20 +0800

    scLeaf

---
+++ javascript/project4/1.视差效果/background.js
@@ -0,0 +1,50 @@
+(function() {
+  var Background = (function() {
+    var that;
+
+    function Background(target) {
+      var ua = navigator.userAgent.toLowerCase();
+      if (/msie 8./.test(ua)) {
+        return;
+      }
+      this.$window = $(window);
+      this.$bgLeaf = $(target);
+      this.init();
+      that = this;
+    }
+
+    Background.prototype.init = function() {
+      return this.bindEvents();
+    };
+
+    Background.prototype.bindEvents = function() {
+      return this.$window.on('scroll',this.parallaxLeaf);
+    };
+
+    Background.prototype.parallaxLeaf = function() {
+      var elem, pos_bg_1, pos_bg_2, pos_bg_3, ref, results, y, y_1, y_2, y_3;
+      
+      y = this.scrollY;
+      ref = that.$bgLeaf;
+      results = [];
+      for (var i = 0; i < ref.length; i++) {
+        elem = ref[i];
+        y_1 = y * .8;
+        y_2 = y * .5;
+        y_3 = y * .3;
+        pos_bg_1 = '0px ' + y_1 + 'px,';
+        pos_bg_2 = '0px ' + y_2 + 'px,';
+        pos_bg_3 = '0px ' + y_3 + 'px';
+        results.push(elem.style.backgroundPosition = pos_bg_1 + pos_bg_2 + pos_bg_3);
+      }
+      return results;
+    };
+
+    return Background;
+
+  })();
+
+  window.ks = window.ks || (ks = {});
+  window.ks.Background = Background || (Background = {});
+
+}).call(this);
